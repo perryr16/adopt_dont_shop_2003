0af9eb18189e69d21d8909158bb79a99dbc0572a
Ross Perry <perryr16@gmail.com>
2020-05-05 19:33:57 -0600

    pets per shelter tested

---
+++ spec/features/shelters/8user_can_view_pets_from_shelter_spec.rb
@@ -0,0 +1,75 @@
+require 'rails_helper'
+
+RSpec.describe "Pets can be seen on shelter pet ", type: :feature do
+  it "can view a pet on shelter pet page" do
+    pet1 = Pet.create(
+      name: "Walter",
+      age: "4",
+      sex: "male",
+      shelter: "Barks and Crafts",
+      image: "https://thesmartcanine.com/wp-content/uploads/2019/09/sealyham-terrier-small-dog.jpg",
+      adoptable: true
+    )
+    pet2 = Pet.create(
+      name: "Harry",
+      age: "14",
+      sex: "female",
+      shelter: "Yarn Care",
+      image: "https://cf.ltkcdn.net/cats/images/std/200777-425x322-kitten_crop.jpg",
+      adoptable: true
+    )
+    pet3 = Pet.create(
+      name: "Penny",
+      age: "17",
+      sex: "female",
+      shelter: "Barks and Crafts",
+      image: "https://ichef.bbci.co.uk/news/976/cpsprodpb/16B90/production/_107427039_gettyimages-636475496.jpg",
+      adoptable: false
+    )
+    pet4 = Pet.create(
+      name: "sebastian",
+      age: "100",
+      sex: "male",
+      shelter: "Barks and Crafts",
+      image: "https://ichef.bbci.co.uk/news/976/cpsprodpb/16B90/production/_107427039_gettyimages-636475496.jpg",
+      adoptable: true
+    )
+    shelter2 = Shelter.create(
+      name: "Barks and Crafts",
+      address: "999 woof Ave",
+      city: "Ruff town",
+      state: "DG",
+      zip: "67890"
+    )
+
+
+
+    visit "/shelters/#{shelter2.id}/pets"
+
+    expect(page).to have_content(pet1.name)
+    expect(page).to have_content(pet1.age)
+    expect(page).to have_content(pet1.sex)
+    expect(page).to have_content(pet1.shelter)
+    expect(page).to have_css("img[src*='https://thesmartcanine.com/wp-content/uploads/2019/09/sealyham-terrier-small-dog.jpg']")
+
+    expect(page).not_to have_content(pet2.name)
+    expect(page).not_to have_content(pet2.age)
+    expect(page).not_to have_content(pet2.sex)
+    expect(page).not_to have_content(pet2.shelter)
+    expect(page).not_to have_css("img[src*='https://cf.ltkcdn.net/cats/images/std/200777-425x322-kitten_crop.jpg']")
+
+    expect(page).not_to have_content(pet3.name)
+    expect(page).not_to have_content(pet3.age)
+    expect(page).not_to have_content(pet3.sex)
+    # expect(page).not_to have_content(pet3.shelter)
+    # expect(page).not_to have_css("img[src*='https://ichef.bbci.co.uk/news/976/cpsprodpb/16B90/production/_107427039_gettyimages-636475496.jpg']")
+
+    expect(page).to have_content(pet4.name)
+    expect(page).to have_content(pet4.age)
+    expect(page).to have_content(pet4.sex)
+    expect(page).to have_content(pet4.shelter)
+    expect(page).to have_css("img[src*='https://ichef.bbci.co.uk/news/976/cpsprodpb/16B90/production/_107427039_gettyimages-636475496.jpg']")
+
+  end
+
+end
