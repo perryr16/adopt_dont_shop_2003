3878f5c8aa7b3f9b992875b8f05d1b4bfbbf2303
Ross Perry <perryr16@gmail.com>
2020-05-05 19:46:35 -0600

    attempted to test links to improve coverage-unsuccessful

---
+++ spec/features/pets/9user_can_show_pet_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "pet show ", type: :feature do
+  it "can show a pet" do
+
+    pet1 = Pet.create(
+      name: "Remy",
+      age: "10",
+      sex: "male",
+      shelter: "the lab",
+      image: "https://thesmartcanine.com/wp-content/uploads/2019/09/labrador-pitbull-mix.jpg"
+    )
+
+    visit "/pets/#{pet1.id}"
+
+
+    expect(page).to have_content(pet1.name)
+    expect(page).to have_content(pet1.age)
+    expect(page).to have_content(pet1.sex)
+    expect(page).to have_content(pet1.shelter)
+    expect(page).to have_css("img[src*='https://thesmartcanine.com/wp-content/uploads/2019/09/labrador-pitbull-mix.jpg']")
+    # expect(page).to have_link(nil, href: "/")
+    # expect(page).to have_link(nil, href: "/shelters")
+    # expect(page).to have_link(nil, href: "/shelters/new")
+    # expect(page).to have_link(nil, href: "/pets")
+    # expect(page).to have_link(nil, href: "/pets/new")
+  end
+
+end
