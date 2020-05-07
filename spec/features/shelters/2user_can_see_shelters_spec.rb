e1472f2818f67b3f4891c42a3ea9ed526900da11
Ross Perry <perryr16@gmail.com>
2020-05-07 16:23:22 -0600

    2 and 3 changed to before each

--- spec/features/shelters/2user_can_see_shelters_spec.rb
+++ spec/features/shelters/2user_can_see_shelters_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Shleters index page", type: :feature do
-  it "can see shelter names" do
-    shelter1 = Shelter.create(
+  before :each do
+    @shelter1 = Shelter.create(
       name: "the lab",
       address: "123 Dog Street",
       city: "Dog Town",
       state: "DO",
       zip: "12345"
     )
-    shelter2 = Shelter.create(
+    @shelter2 = Shelter.create(
       name: "starter kit-en",
       address: "999 Felix Ave",
       city: "Cat Alley",
@@ -17,118 +17,63 @@ RSpec.describe "Shleters index page", type: :feature do
       zip: "67890"
     )
 
-    visit "/shelters"
-
-    expect(page).to have_content(shelter1.name)
-    expect(page).to have_content(shelter2.name)
-  end
-
-  it "can list shelters by alphabetical order" do
-    shelter1 = Shelter.create(
-      name: "Z shelter",
-      address: "999 Felix Ave",
-      city: "Cat Alley",
-      state: "CA",
-      zip: "67890"
-    )
-    shelter2 = Shelter.create(
-      name: "A Shelter",
-      address: "123 Dog Street",
-      city: "Dog Town",
-      state: "DO",
-      zip: "12345"
-    )
-
-
-    visit "/shelters"
-
-    expect(page.all('h2')[-2]).to have_content(shelter1.name)
-    expect(page.all('h2')[-1]).to have_content(shelter2.name)
-    click_link("Sort Shelters by Alpbetical Order")
-
-    expect(page.all('h2')[-1]).to have_content(shelter1.name)
-    expect(page.all('h2')[0]).to have_content(shelter2.name)
-
-
-  end
-
-  it "can list shelters by number of pets" do
-    shelter1 = Shelter.create(
-      name: "Z shelter",
-      address: "999 Felix Ave",
-      city: "Cat Alley",
-      state: "CA",
-      zip: "67890"
-    )
-    shelter2 = Shelter.create(
-      name: "no pets",
-      address: "123 Dog Street",
-      city: "Dog Town",
-      state: "DO",
-      zip: "12345"
-    )
-
-    pet1 = Pet.create(
+    @pet1 = Pet.create(
       name: "many pets",
       age: "4",
       sex: "male",
       description: "a bit mischevious",
 
       image: "https://thesmartcanine.com/wp-content/uploads/2019/09/sealyham-terrier-small-dog.jpg",
-      shelter_id: shelter2.id,
+      shelter_id: @shelter2.id,
       adoptable: true
     )
 
-    pet2 = Pet.create(
+    @pet2 = Pet.create(
       name: "Penny",
       age: "55",
       sex: "female",
       description: "known to eat goose poop",
 
       image: "https://www.rover.com/blog/wp-content/uploads/2019/05/puppy-in-bowl.jpg",
-      shelter_id: shelter2.id,
+      shelter_id: @shelter2.id,
       adoptable: true
     )
-    pet3 = Pet.create(
-      name: "Penny",
-      age: "55",
-      sex: "female",
-      description: "known to eat goose poop",
+  end
+  it "can see shelter names" do
 
-      image: "https://www.rover.com/blog/wp-content/uploads/2019/05/puppy-in-bowl.jpg",
-      shelter_id: shelter2.id,
-      adoptable: true
-    )
-    pet4 = Pet.create(
-      name: "Penny",
-      age: "55",
-      sex: "female",
-      description: "known to eat goose poop",
 
-      image: "https://www.rover.com/blog/wp-content/uploads/2019/05/puppy-in-bowl.jpg",
-      shelter_id: shelter2.id,
-      adoptable: true
-    )
-    pet5 = Pet.create(
-      name: "Penny",
-      age: "55",
-      sex: "female",
-      description: "known to eat goose poop",
+    visit "/shelters"
 
-      image: "https://www.rover.com/blog/wp-content/uploads/2019/05/puppy-in-bowl.jpg",
-      shelter_id: shelter2.id,
-      adoptable: true
-    )
+    expect(page).to have_content(@shelter1.name)
+    expect(page).to have_content(@shelter2.name)
+  end
+
+  it "can list shelters by alphabetical order" do
+
+    visit "/shelters"
+
+    expect(page.all('h2')[-2]).to have_content(@shelter1.name)
+    expect(page.all('h2')[-1]).to have_content(@shelter2.name)
+
+    click_link("Sort Shelters by Alpbetical Order")
+
+    expect(page.all('h2')[-1]).to have_content(@shelter1.name)
+    expect(page.all('h2')[0]).to have_content(@shelter2.name)
+
+  end
+
+  it "can list shelters by number of pets" do
+
 
     visit "/shelters"
 
 
-    expect(page.all('h2')[-2]).to have_content(shelter1.name)
-    expect(page.all('h2')[-1]).to have_content(shelter2.name)
+    expect(page.all('h2')[-2]).to have_content(@shelter1.name)
+    expect(page.all('h2')[-1]).to have_content(@shelter2.name)
     click_link("Sort Shelters by Number of Pets")
 
-    expect(page.all('h2')[-1]).to have_content(shelter1.name)
-    expect(page.all('h2')[0]).to have_content(shelter2.name)
+    expect(page.all('h2')[-1]).to have_content(@shelter1.name)
+    expect(page.all('h2')[0]).to have_content(@shelter2.name)
 
 
 
--- spec/features/shelters/3user_can_see_one_shelter_spec.rb
+++ spec/features/shelters/3user_can_see_one_shelter_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "Shleter page", type: :feature do
-  it "can see shelter name address city state and zip" do
-    shelter1 = Shelter.create(
+
+  before :each do
+    @shelter1 = Shelter.create(
       name: "the lab",
       address: "123 Dog Street",
       city: "Dog Town",
       state: "DO",
       zip: "12345"
     )
-    shelter2 = Shelter.create(
+    @shelter2 = Shelter.create(
       name: "starter kit-en",
       address: "999 Felix Ave",
       city: "Cat Alley",
@@ -17,41 +18,38 @@ RSpec.describe "Shleter page", type: :feature do
       zip: "67890"
     )
 
-    visit "/shelters/#{shelter1.id}"
-    expect(page).to have_content("Name: #{shelter1.name}")
-    expect(page).to have_content("Address: #{shelter1.address}")
-    expect(page).to have_content("City: #{shelter1.city}")
-    expect(page).to have_content("State: #{shelter1.state}")
-    expect(page).to have_content("Zip: #{shelter1.zip}")
-
-
-    visit "/shelters/#{shelter2.id}"
-    expect(page).to have_content(shelter2.name)
-    expect(page).to have_content("Address: #{shelter2.address}")
-    expect(page).to have_content("City: #{shelter2.city}")
-    expect(page).to have_content("State: #{shelter2.state}")
-    expect(page).to have_content("Zip: #{shelter2.zip}")
-
-  end
-
-  it "can go to pet pages" do
-    shelter1 = Shelter.create(
-      name: "the lab",
-      address: "123 Dog Street",
-      city: "Dog Town",
-      state: "DO",
-      zip: "12345"
-    )
-    pet1 = Pet.create(
+    @pet1 = Pet.create(
       name: "Remy",
       age: "10",
       sex: "male",
       image: "https://thesmartcanine.com/wp-content/uploads/2019/09/labrador-pitbull-mix.jpg",
-      shelter_id: shelter1.id
+      shelter_id: @shelter1.id
     )
 
-    visit "/shelters/#{shelter1.id}"
+  end
+  it "can see shelter name address city state and zip" do
 
-    expect(page).to have_link(href: "/shelters/#{shelter1.id}/pets")
+    visit "/shelters/#{@shelter1.id}"
+    expect(page).to have_content("Name: #{@shelter1.name}")
+    expect(page).to have_content("Address: #{@shelter1.address}")
+    expect(page).to have_content("City: #{@shelter1.city}")
+    expect(page).to have_content("State: #{@shelter1.state}")
+    expect(page).to have_content("Zip: #{@shelter1.zip}")
+
+    visit "/shelters/#{@shelter2.id}"
+    expect(page).to have_content(@shelter2.name)
+    expect(page).to have_content("Address: #{@shelter2.address}")
+    expect(page).to have_content("City: #{@shelter2.city}")
+    expect(page).to have_content("State: #{@shelter2.state}")
+    expect(page).to have_content("Zip: #{@shelter2.zip}")
+
+  end
+
+  it "can go to pet pages" do
+
+    visit "/shelters/#{@shelter1.id}"
+
+    click_on("See Adoptable Pets!")
+    expect(current_path).to eq("/shelters/#{@shelter1.id}/pets")
   end
 end
