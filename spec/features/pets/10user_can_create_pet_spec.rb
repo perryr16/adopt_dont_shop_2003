71a0be2137fda49fe7d94ad20177b9bc41042155
Ross Perry <perryr16@gmail.com>
2020-05-10 13:08:03 -0600

    add create pet link to shelter index and shelter show

--- spec/features/pets/10user_can_create_pet_spec.rb
+++ spec/features/pets/10user_can_create_pet_spec.rb
@@ -14,12 +14,11 @@ RSpec.describe "pet create ", type: :feature do
 
   it "can create a pet on pets index" do
     visit "shelters/#{@shelter1.id}/pets"
-
     expect(page).not_to have_content("Remy")
     expect(page).not_to have_content("Approximate age: 10")
     expect(page).not_to have_content("Name of shelter: the lab")
     expect(page).not_to have_css("img[src*='https://thesmartcanine.com/wp-content/uploads/2019/09/labrador-pitbull-mix.jpg']")
-    click_link("New Pet")
+    click_link("Create Pet")
 
 
     fill_in "name", with: "Remy"
@@ -34,5 +33,30 @@ RSpec.describe "pet create ", type: :feature do
     expect(page).to have_css("img[src*='https://thesmartcanine.com/wp-content/uploads/2019/09/labrador-pitbull-mix.jpg']")
     expect(page).to have_content("10")
 end
+
+it "can create a pet on shelter show" do
+  visit "shelters/#{@shelter1.id}"
+save_and_open_page
+  expect(page).not_to have_content("Remy")
+  expect(page).not_to have_content("Approximate age: 10")
+  expect(page).not_to have_content("Name of shelter: the lab")
+  expect(page).not_to have_css("img[src*='https://thesmartcanine.com/wp-content/uploads/2019/09/labrador-pitbull-mix.jpg']")
+  click_link("Create Pet")
+
+
+  fill_in "name", with: "Remy"
+  fill_in "age", with: "10"
+  # choose("male")
+  fill_in "image", with: "https://thesmartcanine.com/wp-content/uploads/2019/09/labrador-pitbull-mix.jpg"
+  click_button "Create Pet"
+
+  expect(page).to have_content("Remy")
+  expect(page).to have_content("10")
+  expect(page).to have_content("the lab")
+  expect(page).to have_css("img[src*='https://thesmartcanine.com/wp-content/uploads/2019/09/labrador-pitbull-mix.jpg']")
+  expect(page).to have_content("10")
+end
+
+
 
 end
